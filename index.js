4a2f757f9a60561d51daf5ebd9828d608d08b7a8
ao6820 <ao6820@gmail.com>
2022-01-15 12:40:21 +0100

    Update index.js

--- index.js
+++ index.js
@@ -3,27 +3,27 @@
 
 let scenes = [
   
-    // Start menu (no 0): name, first button, first button link, secon button, second button link
+    // start menu (scene 0): name, first button, first button link, secon button, second button link
       ['<h1>Roguelike game</h1>','Start game',2,'About',1],
 
-    // About page (no 1)
+    // about page (scene 1)
       ['This is a test game created by Aleksander Olszewski.', 'Back', 0, 'Start game', 3],
 
-    // This is no 2
+    // scene 2
       ['Welcome traveler.', 'Who are you?', 3,'Whatever.', 4],
 
-    // This is no 3
+    // scene 3
       ['I\'m a ghost.', 'Nice.', 4,'Bullshit!', 4],
 
-    // This is no 4
+    // scene 4
       ['Eh. Whatever.', 'Ok, bye.', 5,'Cheers.', 5],
       
-    // This is no 5   
+    // scene 5   
       ['<h2>The End</h2>', 'Start new game', 0, 'About', 1],
 
-/* You can add more scenes above. The template goes like this:
-['scene text', 'first button label', number of the scene the first button links to, 'second button label', number of the scene the second button links to],      
-*/
+    /* You can add more scenes above. The template goes like this:
+    ['scene text', 'first button label', number of the scene the first button links to, 'second button label', number of the scene the second button links to],      
+    */
   
 ];
 
@@ -33,12 +33,21 @@ const button = document.querySelector("#button1");
 const button2 = document.querySelector("#button2");
 const buttonSound = document.querySelector("#buttonSound");
 
-// 
-let music = new Audio('./Vopna.mp3'); 
-let musicActive = false;
+// Music settings
+let music = new Audio('./Vopna.mp3');   // you can change './Vopna.mp3' to any URL with music
+let musicActive = false;                // autoplay is off by default, if you want to change it, simply replace false with true
 
+function setButtonSound() {
+  if (musicActive === true) {
+    buttonSound.innerHTML = "■";
+    music.play();
+  }
+  else {
+    buttonSound.innerHTML = "▶";
+  }
+
+// Game engine ;)
 let currentScene = 0;
-
 
 function goToScene(sceneId) {
   storyboard.innerHTML = '<span>' + scenes[sceneId][0].replace(' ', ' </span><span>') + '</span>';
@@ -48,7 +57,7 @@ function goToScene(sceneId) {
 
 goToScene(currentScene);
 
-// Clicking buttons
+// Setting up clickable buttons
 button.addEventListener("click", button1Clicked);
 button2.addEventListener("click", button2Clicked);
 buttonSound.addEventListener("click", buttonSoundClicked);
@@ -65,14 +74,6 @@ function button2Clicked() {
 
 // Turning music on and off
 function buttonSoundClicked() {
-  if (musicActive === false) {
-    music.play();
-    musicActive = true;
-    buttonSound.innerHTML = "■";
-  }
-  else {
-    music.pause();
-    musicActive = false;
-    buttonSound.innerHTML = "▶";
-  }
+  musicActive != musicActive;
+  setButtonSound();
 }
